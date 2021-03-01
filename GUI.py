a9293278f1761509efa7b8e7aeccc5bdff7c519b
Shubham-Mate <73774843+Shubham-Mate@users.noreply.github.com>
2021-03-01 15:07:18 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -10,14 +10,16 @@ class GUI:
 		self.window.title(title)
 		self.window['bg'] = '#161b22'
 		
-		Label(self.window, text='Number of values', fg='white', bg='#161b22', font=Font(family="Helvetica",size=16)).grid(row=0, padx=10, pady=10)
+		self.font_size = 16
+		
+		Label(self.window, text='Number of values', fg='white', bg='#161b22', font=Font(family="Helvetica",size=self.font_size)).grid(row=0, padx=10, pady=10)
 		
 		self.text_frame = Frame(self.window, width=100, height=50)
 		self.text_frame.grid(row=0, column=1, padx=10, pady=10)
 		
 		self.number = StringVar()
-		self.number_entry = Entry(self.text_frame, textvariable=self.number, font=Font(family="Helvetica",size=16)).grid()
-		self.btn = Button(self.window, text="Start", width = 15, fg='white', bg='#0d1117', font=Font(family="Helvetica",size=16), highlightthickness=0, bd=0, activebackground='#040507', activeforeground='white', command=self.getValues)
+		self.number_entry = Entry(self.text_frame, textvariable=self.number, font=Font(family="Helvetica",size=self.font_size)).grid()
+		self.btn = Button(self.window, text="Start", width = 15, fg='white', bg='#0d1117', font=Font(family="Helvetica",size=self.font_size), highlightthickness=0, bd=0, activebackground='#040507', activeforeground='white', command=self.getValues)
 		self.btn.bind("<Enter>", self.onEnter)
 		self.btn.bind("<Leave>", self.onLeave)
 		self.btn.grid(row=2, columnspan=2, padx=10, pady=10)
